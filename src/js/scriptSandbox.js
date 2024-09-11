3846c6a86a3d37e6dc2636fd0c56c95b746b7b5e
Jonas Monteiro <mfernandes.jonas@gmail.com>
2024-09-11 19:31:33 -0300

    feat: created change dark light mode sandbox page

--- src/js/scriptSandbox.js
+++ src/js/scriptSandbox.js
@@ -16,6 +16,10 @@ const blogDpText = document.getElementById('blogDpText')
 const sandboxDpText = document.getElementById('sandboxDpText')
 const aboutSandbox = document.getElementById("aboutSandbox");
 
+const body = document.body
+const changeMode = document.getElementById('changeMode')
+const heroSandbox = document.getElementById("heroSandbox")
+const sandboxThings = document.getElementById("sandboxThings")
 // const h2Blog = document.getElementById("h2Blog");
 // const pBlog = document.getElementById("pBlog");
 
@@ -105,3 +109,71 @@ changeTextIcon.addEventListener('click', () => {
         
     }
 })
+
+changeMode.addEventListener("click", (event) => {
+    event.preventDefault();
+    const currentColor = window.getComputedStyle(body).backgroundColor;
+  
+    if (currentColor === "rgb(17, 17, 17)") {
+      // 'rgb(17, 17, 17)' é a forma que '#111111' é representada
+      body.style.backgroundColor = "#FFFFFF";
+      aboutBlog.style.color = "#202020";
+      aboutSandbox.style.color = "#202020";
+      AboutMenuTablet.style.color = "#202020";
+      logo.style.color = "#2A2A2A";
+      changeTextIcon.style.color = "#2A2A2A";
+      changeMode.style.color = "#2A2A2A";
+      menu.style.color = "#2A2A2A";
+  
+      aboutDropdown.style.backgroundColor = "#F9F9F9"
+      blogDropdown.style.backgroundColor = "#F9F9F9"
+      sandboxDropdown.style.backgroundColor = "#F9F9F9"
+
+      aboutDpText.style.color = "#111111" 
+      blogDpText.style.color = "#111111" 
+      sandboxDpText.style.color = "#111111" 
+      h2Sandbox.style.color = "#111111" 
+      heroSandbox.style.backgroundColor = "#FFFFFF"
+      sandboxThings.style.backgroundColor = "#FFFFFF"
+      pSandbox.style.color = "#7a7a7a" 
+      textSandbox.style.color = "#7a7a7a"
+
+      cardH2.style.color = "#111111"
+      habitH2.style.color = "#111111"
+      smokingH2.style.color = "#111111"
+
+      pCard1.style.color = "#2A2A2A"
+      pCard2.style.color = "#2A2A2A"
+      pCard3.style.color = "#2A2A2A"
+    } else {
+      body.style.backgroundColor = "#111111";
+      logo.style.color = "#EEEEEE";
+      changeMode.style.color = "#a8a8a8";
+      changeTextIcon.style.color = "#a8a8a8";
+      menu.style.color = "#a8a8a8";
+      AboutMenuTablet.style.color = "#a8a8a8";
+      aboutBlog.style.color = "#a8a8a8";
+      aboutSandbox.style.color = "#a8a8a8";
+  
+      aboutDropdown.style.backgroundColor = "#0A0A0A"
+      blogDropdown.style.backgroundColor = "#0A0A0A"
+      sandboxDropdown.style.backgroundColor = "#0A0A0A"
+  
+      aboutDpText.style.color = "#EEEEEE" 
+      blogDpText.style.color = "#EEEEEE" 
+      sandboxDpText.style.color = "#EEEEEE" 
+      heroSandbox.style.backgroundColor = "#111111"
+      sandboxThings.style.backgroundColor = "#111111"
+      h2Sandbox.style.color = "#EEEEEE" 
+      pSandbox.style.color = "#A8A8A8"
+      textSandbox.style.color = "#5f5f5f" 
+
+      cardH2.style.color = "#EEEEEE"
+      habitH2.style.color = "#EEEEEE"
+      smokingH2.style.color = "#EEEEEE"
+
+      pCard1.style.color = "#A8A8A8"
+      pCard2.style.color = "#A8A8A8"
+      pCard3.style.color = "#A8A8A8"
+    }
+  });
